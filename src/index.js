fe8aa8a754d06c8d14444cf3b89476cd82dc6374
VictorParodi <victor.parodi.montero@gmail.com>
2019-03-15 00:36:25 -0500

    app1 commit 2: refactoring App component from functional to class component

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+class App extends React.Component {
+    render() {
+        window.navigator.geolocation.getCurrentPosition(
+            position => console.log(position),
+            error => console.log(error)
+        );
 
-const App = () => {
-    window.navigator.geolocation.getCurrentPosition(
-        position => console.log(position),
-        error => console.log(error)
-    );
-
-    return (
-        <div>Hello!</div>
-    );
+        return (
+            <div>Latitude: </div>
+        );
+    }
 }
 
 ReactDOM.render(
